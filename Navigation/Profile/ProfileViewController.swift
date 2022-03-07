0bf9d8ef324ddb8d256846eb731d68015f8cd2a6
N.D <idn.not@gmail.com>
2022-03-07 19:07:59 +0300

    iosui-8/2.1: add profile view

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,20 +8,32 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+	private lazy var profileHeaderView: ProfileHeaderView = {
+		let view = ProfileHeaderView()
+		return view
+	}()
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		navigationController?.navigationBar.prefersLargeTitles = false
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+		view.backgroundColor = .lightGray
+
+		let appearance = UINavigationBarAppearance()
+		appearance.configureWithOpaqueBackground()
+		appearance.backgroundColor = .systemGray3
+		navigationItem.standardAppearance = appearance
+		navigationItem.scrollEdgeAppearance = appearance
+		navigationItem.compactAppearance = appearance
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	override func viewWillLayoutSubviews() {
+		view.addSubview(profileHeaderView)
+		profileHeaderView.frame = view.frame
+	}
 }
