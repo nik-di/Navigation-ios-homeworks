32682c2873ce010d04c6b6ee4be1fe0d66dd6855
N.D <idn.not@gmail.com>
2022-03-10 01:38:10 +0300

    iosui-8/2.2: add new design of the profile view

--- Navigation/Profile/ProfileHeader/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeader/ProfileHeaderView.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
-	private var statusText = ""
-
 	@IBOutlet weak var avatar: UIImageView!
 	@IBOutlet weak var profileButton: UIButton!
-	@IBOutlet weak var statusEditField: UITextField!
+	@IBOutlet weak var statusTextField: UITextField!
 	@IBOutlet weak var statusLabel: UILabel!
-	
+	@IBOutlet weak var profileButtonTopAnchor: NSLayoutConstraint!
+
+	private var statusText = "" {
+		willSet {
+			if newValue != "" {
+				profileButton.setTitle("Set status", for: .normal)
+			} else {
+				profileButton.setTitle("Close field", for: .normal)
+			}
+		}
+	}
+	private var textFieldVisible: Bool {
+		return self.statusTextField.layer.opacity == 1
+	}
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		setupView()
@@ -30,58 +42,64 @@ class ProfileHeaderView: UIView {
 		addSubview(view)
 		view.frame = frame
 
+		if let superview = superview {
+			NSLayoutConstraint.activate([
+				view.topAnchor.constraint(equalTo: superview.topAnchor),
+				view.heightAnchor.constraint(equalToConstant: 220)
+			])
+		}
+
 		configureAvatar()
 		configureProfileButton()
 		configureStatusEditField()
 	}
 
 	private func configureAvatar() {
-		avatar.layer.borderWidth = 3
 		avatar.layer.borderColor = UIColor.white.cgColor
 		avatar.layer.cornerRadius = avatar.frame.height / 2
 	}
 
 	private func configureProfileButton() {
-		profileButton.layer.cornerRadius = 4
-		profileButton.layer.shadowRadius = 4
-		profileButton.layer.shadowOpacity = 0.7
-		profileButton.layer.shadowColor = UIColor.black.cgColor
-		profileButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-
-		profileButton.setTitle("Set status", for: .normal)
-
-		profileButton.addTarget(self, action: #selector(animateButton), for: .touchDown)
-		profileButton.addTarget(self, action: #selector(onTapProfileButton), for: .touchUpInside)
+		profileButton.setTitle("Change status", for: .normal)
+		profileButton.addTarget(self, action: #selector(animateButton), for: .touchUpInside)
 	}
 
 	private func configureStatusEditField() {
-		statusEditField.layer.borderWidth = 1
-		statusEditField.layer.cornerRadius = 12
-		statusEditField.layer.borderColor = UIColor.black.cgColor
-		statusEditField.layer.backgroundColor = UIColor.white.cgColor
-		statusEditField.clipsToBounds = true
-
-		statusEditField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+		statusTextField.clipsToBounds = true
+		statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
 	}
 
 	@objc func animateButton(_ button: UIButton) {
+		let closure = onTapProfileButton
 		button.transform = CGAffineTransform(scaleX: 0.95, y: 0.9)
-		UIView.animate(withDuration: 0.15, animations: { button.transform = CGAffineTransform.identity })
+		UIView.animate(withDuration: 0.15, animations: { button.transform = CGAffineTransform.identity }, completion: { isEnd in
+			if isEnd {
+				closure()
+			}
+		})
 	}
 
 	@objc private func onTapProfileButton() {
+		let textFieldVisible = self.textFieldVisible
+		UIView.animate(withDuration: 0.15, animations: { [weak self] in
+			guard self != nil else { return }
+
+			self!.statusTextField.layer.opacity = textFieldVisible ? 0 : 1
+			self!.profileButtonTopAnchor.constant = textFieldVisible ? 16 : 75
+			self!.layoutIfNeeded()
+		})
+
 		if statusText != "" {
 			statusLabel.text = statusText
-			statusEditField.text = ""
-			statusEditField.endEditing(true)
+			statusTextField.text = ""
+			statusTextField.endEditing(true)
 		}
+
+		profileButton.setTitle("Change status", for: .normal)
 	}
 
 	@objc func statusTextChanged(_ textField: UITextField) {
 		guard let text = textField.text else { return }
-
-		if text != "" {
-			statusText = text
-		}
+		statusText = text
 	}
 }
