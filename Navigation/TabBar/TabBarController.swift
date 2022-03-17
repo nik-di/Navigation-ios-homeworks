b77e34369362f12ee01b3029b9e435381a1cf3f6
N.D <idn.not@gmail.com>
2022-03-17 23:18:59 +0300

    iosui-8/2.3: connect login page

--- Navigation/TabBar/TabBarController.swift
+++ Navigation/TabBar/TabBarController.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+	private var isLoggedIn = false
+
+	private var secondViewController: UIViewController {
+		createNavigationController(for: isLoggedIn ? ProfileViewController() : loginVC(), title: "Профиль", image: UIImage(systemName: "person")!)
+	}
+
+	private func switchSecondViewController() {
+		isLoggedIn = true
+		viewControllers?.removeLast()
+		viewControllers?.append(secondViewController)
+		selectedIndex = 1
+	}
+
+	private lazy var loginVC = { () -> LoginViewController in
+		let vc = LoginViewController()
+		vc.completion = self.switchSecondViewController
+		return vc
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +48,7 @@ class TabBarController: UITabBarController {
 	func setupViewControllers() {
 		viewControllers = [
 			createNavigationController(for: FeedViewController(), title: "Лента", image: UIImage(systemName: "house")!),
-			createNavigationController(for: ProfileViewController(), title: "Профиль", image: UIImage(systemName: "person")!)
+			secondViewController
 		]
 	}
 }
